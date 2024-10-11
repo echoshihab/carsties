7c5e566e7e75b1d7fb10ec7cbe91ca059f1c7bcf
Shihab Khan <echoshihab@gmail.com>
2024-10-10 21:50:31 -0400

    seed auction data fetched from auction api

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,10 +1,13 @@
 using SearchService;
+using SearchService.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddHttpClient<AuctionSvcHttpClient>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 
