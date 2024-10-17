ca44b85d8d8dc2381b1dd5c6c6ec93f2d5ba2d45
Shihab Khan <echoshihab@gmail.com>
2024-10-16 21:07:30 -0400

    add automapper configuration and mapping profile to search service

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -10,6 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPollingPolicy());
 builder.Services.AddMassTransit(x =>
 {
