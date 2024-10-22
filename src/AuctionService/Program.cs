22f699e1859c2c478eaaeaa8565bd5cc385d8a2b
Shihab Khan <echoshihab@gmail.com>
2024-10-21 23:09:18 -0400

    add configurations and migrations for masstransit outbox to save and retry failed messages to rabbitmq

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -1,4 +1,5 @@
 using AuctionService.Data;
+using AuctionService.Entities;
 using MassTransit;
 using Microsoft.EntityFrameworkCore;
 
@@ -15,6 +16,13 @@ builder.Services.AddDbContext<AuctionDbContext>(opt =>
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddMassTransit(x =>
 {
+    x.AddEntityFrameworkOutbox<AuctionDbContext>(o =>
+    {
+        o.QueryDelay = TimeSpan.FromSeconds(10);
+        o.UsePostgres();
+        o.UseBusOutbox();
+    });
+
     x.UsingRabbitMq((context, cfg) =>
     {
         cfg.ConfigureEndpoints(context);
